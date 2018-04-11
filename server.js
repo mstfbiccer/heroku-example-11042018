ea42e149368a90486b902c1eef33282528279c06
Mustafa Bicer <mstfbiccer@gmail.com>
2018-04-11 21:56:45 +0300

    Heroku push

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
-app.listen(2019, function () {
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
